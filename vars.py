0a2c0baa27cbbd09424b99a8c7075d6928a15176
Tushar <tusartxt7570@gmail.com>
2025-02-13 18:43:28 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 # Don't Remove Credit Telegram - @Engineers_Babu
 from os import environ
 
-API_ID = int(environ.get("API_ID", "21705536"))
-API_HASH = environ.get("API_HASH", "c5bb241f6e3ecf33fe68a444e288de2d")
+API_ID = int(environ.get("API_ID", "24473318"))
+API_HASH = environ.get("API_HASH", "e7dd0576c5ac0ff8f90971d6bb04c8f5")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
